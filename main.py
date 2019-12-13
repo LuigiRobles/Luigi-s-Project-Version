00e7886083413b74c26fbf0dcd036e92373811c0
LuigiRobles <luigi.robles33@gmail.com>
2019-12-13 06:29:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# Project 01 - Scrabble Game
+
+# Function 1: Input Player Rounds
+def inputRounds():
+    return None
+
+# Function 2: Input Player Word
+def inputWord():
+    return None
+
+# Function 3: Validate Word
+def validWord():
+    return None
+
+# Function 4: Generate Score
+def playerScore():
+    return None
+
+# Function 5: Initialize Messages Tuple
+def initMessages():
+    return None
+
+# Function 6: Initialize Points Dictionary
+def initPoints():
+    return None
+
+# Function 7: Initialize Players Dictionary
+def initPlayers():
+    return None
+
+# Main Program
+if __name__ == '__main__':
+
+    # Declare Variables
+    points = initPoints()
+    players = initPlayers()
+    messages = initMessages()
+
+    # Main Program
+    print('Main Program Goes Here') 
